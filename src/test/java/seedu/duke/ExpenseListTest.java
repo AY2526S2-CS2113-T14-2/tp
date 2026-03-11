e3cfab7ccf98a13e2cff34f853db99ab4ed305d6
Kynaston <weeyuqikynaston@gmail.com>
2026-03-11 17:43:57 +0800

    Added just the empty class with @BeforeEach setup

---
+++ src/test/java/seedu/duke/ExpenseListTest.java
@@ -0,0 +1,24 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.duke.data.ExpenseList;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class ExpenseListTest {
+
+    private ExpenseList expenseList;
+    /**
+     * Initialises a new empty {@link ExpenseList} before each test.
+     * This ensures full isolation between test cases.
+     */
+    @BeforeEach
+    void setUp(){
+        expenseList = new ExpenseList();
+    }
+}
