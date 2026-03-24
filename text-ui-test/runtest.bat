b901b9bac4a5877ba433bd4270da1e432cd88722
Kynaston <weeyuqikynaston@gmail.com>
2026-03-24 20:53:37 +0800

    Updated bort runtest.sh and runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,9 +6,7 @@ cd ..
 call gradlew clean shadowJar
 
 cd build\libs
-for /f "tokens=*" %%a in (
-    'dir /b *.jar'
-) do (
+for /f "tokens=*" %%a in ('dir /b *.jar') do (
     set jarloc=%%a
 )
 
@@ -16,15 +14,10 @@ java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TX
 
 cd ..\..\text-ui-test
 
-findstr /v "remaining" ACTUAL.TXT > ACTUAL_FILTERED.TXT
-findstr /v "remaining" EXPECTED.TXT > EXPECTED_FILTERED.TXT
-
-cmd /c "FC ACTUAL_FILTERED.TXT EXPECTED_FILTERED.TXT >NUL"
+cmd /c "FC ACTUAL.TXT EXPECTED.TXT >NUL"
 if %errorlevel% == 0 (
     echo Test passed!
-    del ACTUAL_FILTERED.TXT EXPECTED_FILTERED.TXT
 ) else (
     echo Test failed!
-    del ACTUAL_FILTERED.TXT EXPECTED_FILTERED.TXT
     exit /b 1
 )
